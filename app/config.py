5d735cbed4082f33fd5778ff9d3daf81dc90c393
Olena <efymenko.olena@gmail.com>
2024-11-18 00:52:27 +0200

    Integrate @lru_cache in the configutation

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 
 from pydantic_settings import BaseSettings
 
@@ -8,9 +9,10 @@ log = logging.getLogger("uvicorn")
 
 class Settings(BaseSettings):
     environment: str = "dev"
-    testing: bool = bool(0)
+    testing: bool = 0
 
 
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
